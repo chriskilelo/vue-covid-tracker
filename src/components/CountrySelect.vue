918a9dfb88574c11df4b9f549510ac13312f7445
chriskilelo <chriskilelo@gmail.com>
2023-09-18 17:57:47 +0300

    Create the 'CountrySelect.vue' component to allow the customer to select the country they wish to check the statistics for

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,28 @@
+<template>
+    <select @change="onChange" v-model="selected" class="form-select mt-10 block w-full border border-green-700 p-3 rounded">
+        <option value="0">Select Country</option>
+        <option v-for="country in countries" :value="country.id" class="text-red-600">
+            {{ country.country }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: [
+        'countries'
+    ],
+    data() {
+        return {
+            selected: 0
+        }
+    },
+    methods: {
+        onChange(){
+            const countryData  = this.countries.find((item) => item.id === this.selected)
+            this.$emit('get-country', countryData )
+        }
+    }
+}
+</script>
